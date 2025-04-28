4c59d2342e432a304e83e7aae20312f6a1551b64
brobear19 <brobearvsgaming@gmail.com>
2025-04-28 00:09:20 -0400

    a
    a

--- API/config/database.js
+++ API/config/database.js
@@ -4,6 +4,12 @@ require('dotenv').config();
 
 const Connection = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   logging: false, // Set to true for SQL query logging
 });
 
